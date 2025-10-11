44a702d94be130ebe1f37ae64e2ac0095325fba2
aleksandra <matealeksandra@gmail.com>
2025-10-11 14:07:51 +0200

    bug fix

--- src/store/useFinanceStore.ts
+++ src/store/useFinanceStore.ts
@@ -1,6 +1,6 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
-import { Transaction, Budget, CreateTransactionInput, CreateBudgetInput } from '../types';
+import type {Budget, CreateBudgetInput, CreateTransactionInput, Transaction} from "../types";
 
 // ============================================
 // ИНТЕРФЕЙС STORE
