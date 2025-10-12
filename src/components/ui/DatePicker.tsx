aa4e8e6dbd9108a1032f1f6a4415d778468b7217
aleksandra <matealeksandra@gmail.com>
2025-10-12 12:46:13 +0200

    add custom DatePicker component using MUI x-date-pickers with dayjs and locale support

---
+++ src/components/ui/DatePicker.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { DatePicker as MuiDatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import dayjs, { Dayjs } from 'dayjs';
+import 'dayjs/locale/ru';
+
+interface DatePickerProps {
+    label: string;
+    value: string;
+    onChange: (date: string) => void;
+    error?: boolean;
+    helperText?: string;
+    required?: boolean;
+}
+
+export const DatePicker: React.FC<DatePickerProps> = ({
+                                                          label,
+                                                          value,
+                                                          onChange,
+                                                          error,
+                                                          helperText,
+                                                          required,
+                                                      }) => {
+    const handleChange = (newValue: Dayjs | null) => {
+        if (newValue) {
+            onChange(newValue.format('YYYY-MM-DD'));
+        }
+    };
+
+    return (
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="ru">
+            <MuiDatePicker
+                label={label}
+                value={value ? dayjs(value) : null}
+                onChange={handleChange}
+                slotProps={{
+                    textField: {
+                        fullWidth: true,
+                        required,
+                        error,
+                        helperText,
+                    },
+                }}
+            />
+        </LocalizationProvider>
+    );
+};
