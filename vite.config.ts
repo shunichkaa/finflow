d5ce51f2a891a48c6aec38d876d1f0cd27c4ce6a
aleksandra <matealeksandra@gmail.com>
2025-10-14 15:29:05 +0200

    remove react-buddy

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
     plugins: [react()],
     build: {
+<<<<<<< Current (Your changes)
+        outDir: 'dist',
+        assetsDir: 'assets'
+=======
         rollupOptions: {
-            input: '/src/main.tsx',
+            input: './index.html',
         },
+>>>>>>> Incoming (Background Agent changes)
     },
+    base: './'
 });
