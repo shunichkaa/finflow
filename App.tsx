d5a92b3350a8fe3ceaf87693f21a7d83b93ad81b
aleksandra <matealeksandra@gmail.com>
2025-10-15 10:22:25 +0200

    Added route for analytics page in the router

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import Budgets from "./src/BudgetList/pages/Budgets";
 import {Layout} from "./src/components/Layout";
-import {Analytics} from "./src/BudgetList/pages/Analytics";
 import Dashboard from "./src/BudgetList/pages/Dashboard.tsx";
+import Analytics from "./src/BudgetList/pages/Analytics.tsx";
 
 function App() {
     return (
@@ -11,7 +11,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Navigate to="/dashboard" replace/>}/>
                     <Route path="/dashboard" element={<Dashboard/>}/>
-                    <Route path="/analytics" element={<Analytics/>}/>
+                    <Route path="/analytics" element={<Analytics />} />
                     <Route path="/budgets" element={<Budgets/>}/>
                     <Route path="*" element={<Navigate to="/dashboard" replace/>}/>
                 </Routes>
