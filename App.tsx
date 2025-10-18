ecd80e2ed0c1c90f81ea584f8197c3634d53f1d9
aleksandra <matealeksandra@gmail.com>
2025-10-18 11:20:14 +0200

    bug fix

--- App.tsx
+++ App.tsx
@@ -1,26 +1,26 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import {BrowserRouter, Navigate, Route, Routes} from 'react-router-dom';
 import Budgets from "./src/BudgetList/pages/Budgets";
 import Dashboard from "./src/BudgetList/pages/Dashboard";
 import Analytics from "./src/BudgetList/pages/Analytics";
-import { Auth } from "./src/components/auth/Auth";
-import { ProtectedRoute } from "./src/components/auth/ProtectedRoute";
-import { Layout } from "./src/components/Layout";
+import {Auth} from "./src/components/auth/Auth";
+import {ProtectedRoute} from "./src/components/auth/ProtectedRoute";
+import {Layout} from "./src/components/Layout";
 import {OAuthCallback} from "./src/components/auth/OAuthCallback.tsx";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/login" element={<Auth />} />
-                <Route path="/oauth-callback" element={<OAuthCallback />} />
+                <Route path="/login" element={<Auth/>}/>
+                <Route path="/oauth-callback" element={<OAuthCallback/>}/>
 
-                <Route path="/" element={<Layout defaultSidebarOpen={true} />}>
-                    <Route index element={<Navigate to="/dashboard" replace />} />
+                <Route path="/" element={<Layout defaultSidebarOpen={true}/>}>
+                    <Route index element={<Navigate to="/dashboard" replace/>}/>
                     <Route
                         path="dashboard"
                         element={
                             <ProtectedRoute>
-                                <Dashboard />
+                                <Dashboard/>
                             </ProtectedRoute>
                         }
                     />
@@ -28,7 +28,7 @@ function App() {
                         path="analytics"
                         element={
                             <ProtectedRoute>
-                                <Analytics />
+                                <Analytics/>
                             </ProtectedRoute>
                         }
                     />
@@ -36,13 +36,13 @@ function App() {
                         path="budgets"
                         element={
                             <ProtectedRoute>
-                                <Budgets />
+                                <Budgets/>
                             </ProtectedRoute>
                         }
                     />
                 </Route>
 
-                <Route path="*" element={<Navigate to="/dashboard" replace />} />
+                <Route path="*" element={<Navigate to="/dashboard" replace/>}/>
             </Routes>
         </BrowserRouter>
     );
