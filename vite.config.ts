aa2adb7358ebf6723ec3e3f0ea345bdc1ca5fad3
aleksandra <matealeksandra@gmail.com>
2025-10-18 12:45:35 +0200

    login fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
     build: {
-        outDir: 'dist'
+        outDir: 'dist',
+        sourcemap: false
     },
-    base: '/'
-});
+    server: {
+        port: 5173
+    }
+})
